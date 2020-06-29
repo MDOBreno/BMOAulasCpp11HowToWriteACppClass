cb9936925a916baebdf85050c53afa22ce63c045
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-06-29 00:18:56 -0300

    - Aula concluida.

--- BMOAulasCpp11HowToWriteACppClass/main.cpp
+++ BMOAulasCpp11HowToWriteACppClass/main.cpp
@@ -8,8 +8,45 @@
 
 #include <iostream>
 
+class Log {
+public:
+    const int logDeLevelErro = 0;
+    const int logDeLevelAlerta = 1;
+    const int logDeLevelInformacao = 2;
+    
+private:
+    int m_logDeLevel = logDeLevelInformacao;
+    
+public:
+    void SetLevel(int level) {
+        m_logDeLevel = level;
+    }
+    
+    void Erro(const char* mensagem) {
+        if (m_logDeLevel >= logDeLevelErro) {
+            std::cout << "[ERRO]: " << mensagem << std::endl;
+        }
+    }
+    void Alerta(const char* mensagem) {
+        if (m_logDeLevel >= logDeLevelAlerta) {
+            std::cout << "[ALERTA]: " << mensagem << std::endl;
+        }
+    }
+    void Informacao(const char* mensagem) {
+        if (m_logDeLevel >= logDeLevelInformacao) {
+            std::cout << "[INFORMAÇÃO]: " << mensagem << std::endl;
+        }
+    }
+};
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    Log log;
+    //log.SetLevel(log.logDeLevelAlerta);   //Caso não definamos(descomentemos), o level será '2'.
+    log.Alerta("Hello!");
+    
+    log.Erro("Hello!");
+    log.Informacao("Hello!");
+    
     return 0;
 }
